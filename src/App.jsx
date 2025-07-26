77bf18f9efa80d0b78818239fac19ef7a11fb58b
Parth <khannaparth846@gmail.com>
2025-07-27 00:27:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+  // let counter = 19;
+
+  const addValue = () => {
+    // counter = counter + 1;
+    setCounter(counter + 1);
+    console.log("clicked", counter);
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}> Add value {counter}</button>
+      <button onClick={removeValue}>Remove value{counter}</button>
+    </>
+  );
+}
+
+export default App;
